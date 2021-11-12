3925ab1b1ecd8ce4de656f644c4086fd4f1346fc
leandro amaral <kaoany8@gmail.com>
2021-11-12 19:52:33 -0400

    finalizada a função para baixar videos

--- youtube_video_inf.py
+++ youtube_video_inf.py
@@ -1,3 +1,15 @@
 from youtube_dl import YoutubeDL
 import sys
 import time
+
+def baixar_info_video(video_id):
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+    youtube_dl_options = {}
+    with YoutubeDL(youtube_dl_options) as ydl:
+        info_dict = ydl.extract_info(video_url, download = False)
+        return info_dict
+
+if __name__ == "__main__":
+    video1 = "CXZlO6gwTSA"
+    info = baixar_info_video(video1)
+    print(info)
